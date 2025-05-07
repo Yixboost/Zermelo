3e8009155b5428bf303c5b16535b3f7ec1f4ceb5
Jonasvanleeuwen19 <jonasvanleeuwen19@duck.com>
2025-05-07 09:43:19 +0200

    Add fullcalendar widgets (example)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -38,5 +38,84 @@ window.addEventListener("DOMContentLoaded", () => {
   }, 1100);
 });
 
-// --- Optionally: sidebar nav coolness (leave for future) ---
-// For more complex interactivity, add event listeners here
+// --- Startpage calendar list ---
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('list-calendar');
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    locale: 'nl',
+    initialView: 'listWeek',
+    height: '100%',
+    headerToolbar: {
+      start: 'prev,next today',
+      center: 'title',
+      end: ''
+    },
+    buttonText: {
+      today: 'Vandaag'
+    },
+    views: {
+      listWeek: {
+        noEventsText: 'Geen resultaten',
+      }
+    },
+    allDayText: 'Hele dag',
+    events: [
+      {
+        title: 'Voorbeeld event',
+        start: new Date().toISOString().split('T')[0]
+      }
+    ]
+  });
+  calendar.render();
+});
+
+// --- Big calendar ---
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('calendar');
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'timeGridWeek',
+    nowIndicator: true,
+    firstDay: 1,
+    hiddenDays: [0, 6],
+    locale: 'nl',
+    height: '100%',
+    headerToolbar: {
+      start: 'timeGridWeek,timeGridDay', 
+      center: 'title',
+      end: 'today prev,next'
+    },
+    buttonText: {
+      today: 'Vandaag',
+      week: 'Week',
+      day: 'Dag'
+    },
+    events: [
+      {
+        title: 'Wiskunde toets',
+        start: new Date().toISOString().split('T')[0] + 'T09:00:00',
+        end: new Date().toISOString().split('T')[0] + 'T10:00:00'
+      },
+      {
+        title: 'Gym',
+        start: new Date().toISOString().split('T')[0] + 'T13:00:00',
+        end: new Date().toISOString().split('T')[0] + 'T14:00:00'
+      },
+      {
+        title: 'Engels (vervallen)',
+        start: new Date().toISOString().split('T')[0] + 'T10:00:00',
+        end: new Date().toISOString().split('T')[0] + 'T11:00:00',
+        color: 'red',
+        textColor: 'white'
+      }
+    ],
+    allDayText: 'Hele dag',
+    noEventsContent: 'Geen resultaten',
+    slotDuration: '00:30:00',
+    slotLabelInterval: '01:00', 
+    slotLabelFormat: { hour: 'numeric', minute: '2-digit', hour12: false },
+    slotMinTime: '08:00:00', 
+    slotMaxTime: '19:00:00' 
+  });
+
+  calendar.render();
+});
